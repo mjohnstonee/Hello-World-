8d89c5d91cabe94103a46d4358713098982a93bb
Michelle <mjohnstonee@users.noreply.github.com>
2019-09-17 19:20:17 -0400

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,15 @@
+echo "# Hello-World-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/mjohnstonee/Hello-World-.git
+git push -u origin master
+
+public class main {
+public static void main (String [] args) {
+
+System.out.println("This will be printed") ;
+
+}
+
+} 
